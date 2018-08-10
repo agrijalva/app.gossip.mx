f71c40e12678a5b0e2856c1b3205834d1c114a89
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-08-10 13:04:14 -0500

    generoPageFunctions

--- src/pages/genero/genero.ts
+++ src/pages/genero/genero.ts
@@ -10,16 +10,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-genero',
-  templateUrl: 'genero.html',
+	selector: 'page-genero',
+	templateUrl: 'genero.html',
 })
 export class GeneroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	public imageM = '../../assets/imgs/genero/sex_mujer_off.png';
+	public imageH = '../../assets/imgs/genero/sex_hombre_off.png';
+	public m: boolean = false;
+	public h: boolean = false;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GeneroPage');
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	}
 
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad GeneroPage');
+	}
+
+	switchMujer() {
+		if (!this.m && !this.h) {
+			this.imageM = '../../assets/imgs/genero/sex_mujer_on.png';
+			this.imageH = '../../assets/imgs/genero/sex_hombre_off.png';
+		} else if (this.m && !this.h) {
+			this.imageM = '../../assets/imgs/genero/sex_mujer_on.png';
+			this.imageH = '../../assets/imgs/genero/sex_hombre_off.png';
+		} else if (!this.m && this.h) {
+			this.imageM = '../../assets/imgs/genero/sex_mujer_on.png';
+			this.imageH = '../../assets/imgs/genero/sex_hombre_off.png';
+		};
+	};
+
+	swicthHombre() {
+		if (!this.h && !this.m) {
+			this.imageH = '../../assets/imgs/genero/sex_hombre_on.png';
+			this.imageM = '../../assets/imgs/genero/sex_mujer_off.png';
+		} else if (this.h && !this.m) {
+			this.imageH = '../../assets/imgs/genero/sex_hombre_on.png';
+			this.imageM = '../../assets/imgs/genero/sex_mujer_off.png';
+		} else if (!this.h && this.m) {
+			this.imageH = '../../assets/imgs/genero/sex_hombre_on.png';
+			this.imageM = '../../assets/imgs/genero/sex_mujer_off.png';
+		};
+	};
 }
